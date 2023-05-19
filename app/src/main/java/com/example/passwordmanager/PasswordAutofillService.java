aa792d5ab287d9f008973164618fba0b5f455eb8
eugen.casian <casian.eugen1999@gmail.com>
2023-05-19 13:18:41 +0300

    Adding timestamp for new elements

--- app/src/main/java/com/example/passwordmanager/PasswordAutofillService.java
+++ app/src/main/java/com/example/passwordmanager/PasswordAutofillService.java
@@ -5,21 +5,59 @@ import android.os.CancellationSignal;
 import android.service.autofill.AutofillService;
 import android.service.autofill.Dataset;
 import android.service.autofill.FillCallback;
+import android.service.autofill.FillContext;
 import android.service.autofill.FillRequest;
 import android.service.autofill.FillResponse;
 import android.service.autofill.SaveCallback;
+import android.service.autofill.SaveInfo;
 import android.service.autofill.SaveRequest;
+import android.view.autofill.AutofillId;
 import android.view.autofill.AutofillValue;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
 import java.util.ArrayList;
+import java.util.List;
 
 public class PasswordAutofillService extends AutofillService {
-
+    private static final String TAG = "MyAutofillService";
     @Override
     public void onFillRequest(@NonNull FillRequest request, @NonNull CancellationSignal cancellationSignal, @NonNull FillCallback callback) {
 
+        FillContext fillContext = request.getFillContexts().get(request.getFillContexts().size() - 1);
+        AssistStructure structure = fillContext.getStructure();
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = mAuth.getCurrentUser();
+        CollectionReference collectionRef;
+        if (user != null){
+            collectionRef = db.collection("users")
+                    .document(user.getUid())
+                    .collection("passwords");
+            collectionRef.get().addOnCompleteListener(task -> {
+                if (task.isSuccessful()) {
+                    QuerySnapshot querySnapshot = task.getResult();
+                    if (querySnapshot != null) {
+                        List<DocumentSnapshot> documents = querySnapshot.getDocuments();
+                        for (DocumentSnapshot document : documents) {
+                            String username = document.getString("5)Username");
+                            String password = document.getString("6)Password");
+
+                        }
+                    }
+                }
+            });
+        }
     }
 
     @Override
